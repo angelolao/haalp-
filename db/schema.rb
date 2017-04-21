5c38306aa942c1f97e3a3d3a2f69d689d9ec57d1
angelolao <angelolao.08@gmail.com>
2017-04-21 19:21:27 +0800

    add task price and number

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421104022) do
+ActiveRecord::Schema.define(version: 20170421112046) do
 
   create_table "category", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title",       null: false
@@ -33,16 +33,18 @@ ActiveRecord::Schema.define(version: 20170421104022) do
   end
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "poster_id",    null: false
-    t.integer  "status",       null: false
-    t.string   "title",        null: false
-    t.string   "due_date",     null: false
-    t.string   "description",  null: false
-    t.string   "location",     null: false
+    t.integer  "poster_id",                    null: false
+    t.integer  "status",                       null: false
+    t.string   "title",                        null: false
+    t.string   "due_date",                     null: false
+    t.string   "description",                  null: false
+    t.string   "location",                     null: false
     t.string   "category_ids"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.integer  "worker_id"
+    t.decimal  "price",         precision: 10
+    t.integer  "worker_number"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
