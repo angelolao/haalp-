c36280684916ab0484e678bb91870509478519c8
angelolao <angelolao.08@gmail.com>
2017-04-21 17:17:49 +0800

    add type and status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421073330) do
+ActiveRecord::Schema.define(version: 20170421091704) do
 
   create_table "category", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title",       null: false
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 20170421073330) do
     t.string   "contact"
     t.string   "skype"
     t.string   "image"
+    t.string   "type",                                null: false
+    t.string   "status"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
