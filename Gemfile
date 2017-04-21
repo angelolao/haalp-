d06f17a4039a60add8742ead879223f6aa9d6ced
angelolao <angelolao.08@gmail.com>
2017-04-22 03:53:05 +0800

    add mock payment and alertify

--- Gemfile
+++ Gemfile
@@ -68,4 +68,5 @@ gem 'omniauth-facebook'
 gem "figaro"
 gem 'cancancan', '~> 1.10'
 gem 'simple_form'
-gem "font-awesome-rails"
+gem 'font-awesome-rails'
+gem 'alertify-rails'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421135225) do
+ActiveRecord::Schema.define(version: 20170421183809) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title",       null: false
@@ -51,12 +51,12 @@ ActiveRecord::Schema.define(version: 20170421135225) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                                 default: "", null: false
+    t.string   "encrypted_password",                    default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                         default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -68,10 +68,11 @@ ActiveRecord::Schema.define(version: 20170421135225) do
     t.string   "status"
     t.string   "user_type"
     t.string   "uid"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
     t.string   "provider"
+    t.datetime "created_at",                                         null: false
+    t.datetime "updated_at",                                         null: false
     t.string   "category_ids"
+    t.decimal  "money",                  precision: 10, default: 0
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
