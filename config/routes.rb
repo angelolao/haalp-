58fc30770067b30dfaf018105779280e215f6d99
Carl Joshua M. Deiparine <carl.deiparine@bibo.com.ph>
2017-04-21 17:58:05 +0800

    workers and posters

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   get 'about_us', to: 'pages#about'
   get 'contact_us', to: 'pages#contact'
   get 'corporate', to: 'pages#corporate'
+
+  resources :users, only: [:index]
+
 end
