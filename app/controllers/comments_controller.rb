8c895433cdf622d5283e4ee5eb79cffd6d43b301
jebie <jebiecorsiga@ymail.com>
2017-04-22 13:11:05 +0800

    fix add comment to the workers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,20 @@
 class CommentsController < ApplicationController
   def create
-    if Comment.create(permit_parameters)
-      flash[:notice] = "Comment sent"
-    else
-      flash[:error] = "Something went wrong"
-    end
+    create_comment
     redirect_to task_path(params[:comment][:task_id])
   end
 
   private
 
-  def permit_parameters
+  def create_comment
+    offers = Offer.search(task_id: params[:comment][:task_id], status: "accepted")
+    offers.each do |offer|
+      Comment.create(permit_parameters(offer.user_id))
+    end
+  end
+
+  def permit_parameters(user_id)
+    params[:comment][:user_id] = user_id
     params.require(:comment).permit(
       :user_id,
       :task_id,
